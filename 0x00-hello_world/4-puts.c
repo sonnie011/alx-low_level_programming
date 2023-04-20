d9706bc471d79b9c3fe05aa293e264e090d8be5c
sonnie011 <gracesonny67@gmail.com>
2023-04-20 06:59:13 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,16 @@
-#!/bin/bash
-# a first program in c
-*file : 4-puts.c
-#include<stdio.h>
+ * File: 4-puts.c
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-printf("\n Hello World \n");
-return 0;
+	puts("\"Programming is like building a multilingual puzzle");
 
+	return (0);
 }
-
