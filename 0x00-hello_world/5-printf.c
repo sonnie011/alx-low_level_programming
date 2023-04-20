26f15e275b0c8f9a2b82e0a954f0efd3015540b7
sonnie011 <gracesonny67@gmail.com>
2023-04-20 12:37:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Auth: grace muthoni n <375@bettyschool.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Printf "with proper grammar, but the outcome is a piece of art
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf.c("\"with proper grammar, but the outcome is a piece of art");
+
+	return (0);
+}
