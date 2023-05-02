f1ff931b6403a3117114370b58f162e4821ef6d7
sonnie011 <gracesonny67@gmail.com>
2023-05-02 06:04:52 -0700

    c arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - function with two int type pointer argument
+ * @a: 1st argument int type
+ * @b: 2nd argument int type
+ *
+ * main: using pointer to change value of q and b
+ * Return: void
+ */
+void swap_int(int *a, int *b);
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
